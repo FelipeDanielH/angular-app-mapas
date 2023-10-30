a1c216465377f30adc3b2f866bdca621cafbb8c9
Felipe <felipe.daniel.henriquez@gmail.com>
2023-10-30 16:32:47 -0300

    feat(side menu): agrega side menu

--- src/app/maps/components/side-menu/side-menu.component.ts
+++ src/app/maps/components/side-menu/side-menu.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
+interface MenuItem {
+  name: string;
+  route: string;
+}
 @Component({
-  selector: 'app-side-menu',
+  selector: 'maps-side-menu',
   templateUrl: './side-menu.component.html',
   styleUrls: ['./side-menu.component.css']
 })
 export class SideMenuComponent {
-
+  public menuItems: MenuItem[] = [
+    { route: '/maps/fullscreen', name: 'Full-Screen'},
+    { route: '/maps/zoom-range', name: 'Zoom-Range'},
+    { route: '/maps/markers', name: 'Markers'},
+    {route: '/maps/properties', name: 'Houses'},
+  ];
 }
