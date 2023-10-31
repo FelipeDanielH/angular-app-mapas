90aef039af49aef9b525b49e32a3c266bc217e1a
Felipe <felipe.daniel.henriquez@gmail.com>
2023-10-31 11:52:15 -0300

    feat(zoom range page): agrega funcionalidad al slider

--- src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
+++ src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
@@ -9,17 +9,48 @@ export class ZoomRangePageComponent implements AfterViewInit {
 
   @ViewChild('map') divMap?: ElementRef
 
+  public zoom: number = 10;
+  public map?: Map;
+
   ngAfterViewInit(): void {
 
     if ( !this.divMap ) throw 'El elemento HTML no fue encontrado'
 
-    const map = new Map({
+    this.map = new Map({
       container: this.divMap.nativeElement, // container ID
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
       center: [-74.5, 40], // starting position [lng, lat]
-      zoom: 9, // starting zoom
+      zoom: this.zoom, // starting zoom
     });
+
+    this.mapListeners();
   }
 
+  mapListeners():void {
+    if( !this.map ) throw 'Mapa no inicializado';
+
+    this.map.on('zoom', (ev) => {
+      this.zoom = this.map!.getZoom();
+    })
+
+    this.map.on('zoomend', (ev) => {
+      if( this.map!.getZoom() < 18 ) return;
+      this.map!.zoomTo(18);
+    })
+
+  }
+
+  zoomIn(){
+    this.map?.zoomIn();
+  }
+
+  zoomOut(){
+    this.map?.zoomOut();
+  }
+
+  zoomChanged( value: string) {
+    this.zoom = Number(value);
+    this.map?.zoomTo(this.zoom);
+  }
 
 }
