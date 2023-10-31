57cd616d83a287ddbccd054f40fe3f97b3ad061f
Felipe <felipe.daniel.henriquez@gmail.com>
2023-10-31 20:15:42 -0300

    feat(markers): funcion de eliminacion añadida

--- src/app/maps/pages/markers-page/markers-page.component.ts
+++ src/app/maps/pages/markers-page/markers-page.component.ts
@@ -1,6 +1,11 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { LngLat, Map, Marker } from 'mapbox-gl';
 
+interface MarkerAndColor {
+  color: string;
+  marker: Marker;
+}
+
 @Component({
   templateUrl: './markers-page.component.html',
   styleUrls: ['./markers-page.component.css']
@@ -9,12 +14,13 @@ export class MarkersPageComponent {
 
   @ViewChild('map') divMap?: ElementRef;
 
+  public markers: MarkerAndColor[] = [];
 
   public map?: Map;
-  public currentLngLat: LngLat = new LngLat(-70.755,-33.355)
+  public currentLngLat: LngLat = new LngLat(-70.755, -33.355)
 
   ngAfterViewInit(): void {
-    if( !this.divMap ) throw 'El elemento HTML no fue encontrado';
+    if (!this.divMap) throw 'El elemento HTML no fue encontrado';
 
     this.map = new Map({
       container: this.divMap.nativeElement,
@@ -25,38 +31,45 @@ export class MarkersPageComponent {
 
   }
 
-    createMarker(){
+  createMarker() {
 
-      if ( !this.map ) return;
+    if (!this.map) return;
 
-      const color = '#xxxxxx'.replace(/x/g, y=>(Math.random()*16|0).toString(16));
-      const lngLat = this.map.getCenter();
+    const color = '#xxxxxx'.replace(/x/g, y => (Math.random() * 16 | 0).toString(16));
+    const lngLat = this.map.getCenter();
 
-      this.addMarker( lngLat, color );
-    }
+    this.addMarker(lngLat, color);
+  }
 
-    addMarker( lngLat: LngLat, color: string ){
-      if( !this.map ) return;
-
-      const marker = new Marker({
-        color,
-        draggable: true
-      })
-      .setLngLat( lngLat )
-      .addTo( this.map )
-    }
-
-    //referencia para crear markers por codigo
-    /* // para modificar el marker
-    const markerHtml = document. createElement('div');
-    markerHtml.innerHTML = "Felipe"
+  addMarker(lngLat: LngLat, color: string) {
+    if (!this.map) return;
 
     const marker = new Marker({
-      element: markerHtml
+      color,
+      draggable: true
     })
-      .setLngLat( this.currentLngLat )
-      .addTo( this.map ) */
+      .setLngLat(lngLat)
+      .addTo(this.map)
 
+    this.markers.push({
+      color,
+      marker
+    });
+  }
 
-
+  deleteMarker(index: number) {
+    this.markers[index].marker.remove()
+    this.markers.splice(index, 1)
+  }
 }
+
+//referencia para crear markers por codigo
+/* // para modificar el marker
+const markerHtml = document. createElement('div');
+markerHtml.innerHTML = "Felipe"
+
+const marker = new Marker({
+  element: markerHtml
+})
+  .setLngLat( this.currentLngLat )
+  .addTo( this.map ) */
