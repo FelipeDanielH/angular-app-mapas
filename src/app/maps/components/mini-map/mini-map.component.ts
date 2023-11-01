547b03ebdee22957d9e734a8b204006f4117fd2e
Felipe <felipe.daniel.henriquez@gmail.com>
2023-11-01 12:36:20 -0300

    feat(minimap): añade pagina de propiedades

--- src/app/maps/components/mini-map/mini-map.component.ts
+++ src/app/maps/components/mini-map/mini-map.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, Input, ViewChild, ElementRef } from '@angular/core';
+import { Map, Marker } from 'mapbox-gl';
 
 @Component({
-  selector: 'app-mini-map',
+  selector: 'map-mini-map',
   templateUrl: './mini-map.component.html',
   styleUrls: ['./mini-map.component.css']
 })
-export class MiniMapComponent {
+export class MiniMapComponent implements AfterViewInit{
+
+  @Input() lngLat?: [number, number];
+  @ViewChild('map') divMap?: ElementRef;
+
+  public map?: Map;
+
+
+  ngAfterViewInit(): void {
+    if(!this.divMap?.nativeElement) throw "Map div not found";
+    if( !this.lngLat ) throw "LngLat can't be null";
+
+    this.map = new Map({
+      container: this.divMap.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v12',
+      center: this.lngLat,
+      zoom: 15,
+      interactive: false
+    })
+
+    const color = '#xxxxxx'.replace(/x/g, y => (Math.random() * 16 | 0).toString(16));
+
+    const marker = new Marker({
+      color
+    })
+      .setLngLat(this.lngLat)
+      .addTo(this.map)
+  }
+
 
 }
