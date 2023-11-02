437e13b6d535a8ff4ee137de6f0c691dcb397266
Felipe <felipe.daniel.henriquez@gmail.com>
2023-11-02 07:12:36 -0300

    feat(standalone page): agrega standalone page para practicar

---
+++ src/app/alone/pages/alone-page/alone-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './alone-page.component.html',
+  styleUrls: ['./alone-page.component.css']
+})
+export class AlonePageComponent {
+
+}
